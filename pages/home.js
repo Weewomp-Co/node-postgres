1fe70021e1e55bf10089d2a4161eae9a1afa5c9e
weewomp1 <103136665+weewomp1@users.noreply.github.com>
2022-10-29 00:43:35 -0400

    (MAIN) - new DB

--- pages/home.js
+++
@@ -1,7 +0,0 @@
-const logbutton = document.getElementById('logbutton')
-
-const getAllPosts = async () => {
-  console.log('retrieving posts...')
-}
-
-logbutton.addEventListener('click', getAllPosts());
